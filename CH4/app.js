7aa7872d52f7219852e106b258256d072e153b4a
emon22830 <mdemon22830@gmail.com>
2025-09-21 12:54:58 +0800

    CH5 Completed

---
+++ CH4/app.js
@@ -0,0 +1,15 @@
+const greet1 = require("./greet1")
+const greet2 = require("./greet2")
+
+greet1();
+console.log(greet2.greet2())
+
+
+// const app = function(){
+//     console.log("Eating Pizaaa")
+// }
+
+
+// module.exports = {
+//     app:app
+// }
---
+++ CH4/greet2.js
@@ -0,0 +1,5 @@
+
+
+module.exports.greeteee = function(){
+    console.log("Eating Pizza");
+}
